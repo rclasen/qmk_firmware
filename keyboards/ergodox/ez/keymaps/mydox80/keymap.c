4b55277006241a1a1c9c7ee45cdaa2aca88e44b6
Rainer Clasen <bj@zuto.de>
2016-09-30 15:45:57 +0200

    use center keys, again.

--- keyboards/ergodox/ez/keymaps/mydox80/keymap.c
+++ keyboards/ergodox/ez/keymaps/mydox80/keymap.c
@@ -150,7 +150,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * |   `    |   1  |   2  |   3  |   4  |   5  |  Del |           | Print|   6  |   7  |   8  |   9  |   0  |   -    |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * | Tab    |   Q  |   W  |   E  |   R  |   T  |      |           |      |   Y  |   U  |   I  |   O  |   P  |   =    |
+ * | Tab    |   Q  |   W  |   E  |   R  |   T  | Menu |           | fNAV |   Y  |   U  |   I  |   O  |   P  |   =    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | fNAV   |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |   '    |
  * |--------+------+------+------+------+------|  Ins |           | Tab  |------+------+------+------+------+--------|
@@ -161,33 +161,33 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                        ,-------------.       ,-------------.
  *                                        |      |      |       |      |      |
  *                                 ,------|------|------|       |------+------+------.
- *                                 | LCtrl| BSPC |      |       |      | Enter| Space|
+ *                                 | Space| LCtrl|      |       |      | Enter| Space|
  *                                 +      +      +------+       +------+      +      +
- *                                 | Space| BSPC | LSYM |       | RCtrl| Enter| Space|
+ *                                 | Space| LCtrl| BSPC |       |ct/bs | Enter| Space|
  *                                 `--------------------'       `--------------------'
  */
 [BASE] = KEYMAP_80(  // layer 0 : default
         // left hand
         KC_GRV,         KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           KC_DEL,
-        KC_TAB,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           KC_NO,
+        KC_TAB,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           KC_APP,
         LNAV,           KC_A,           KC_S,           KC_D,           KC_F,           KC_G,
         KC_LSFT,        KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,           KC_INS,
         MO(SYM),        TG(MOS),        KC_LGUI,        CMG,            ALT_T(KC_ESC),
                                                                         // left thumb
-                                                                                        KC_NO,          KC_APP,
-                                                                        CTL_T(KC_SPC),  KC_BSPC,        LSYM,
-                                                                        CTL_T(KC_SPC),  KC_BSPC,        LSYM,
+                                                                                        KC_NO,          KC_NO,
+                                                                        KC_SPC,         KC_LCTL,        KC_NO,
+                                                                        KC_SPC,         KC_LCTL,        KC_BSPC,
 
         // right hand
         KC_PSCR,        KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_MINS,
-        KC_NO,          KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_EQL,
+        LNAV,           KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_EQL,
                         KC_H,           KC_J,           KC_K,           KC_L,           KC_SCLN,        KC_QUOT,
         KC_TAB,         KC_N,           KC_M,           KC_COMM,        KC_DOT,         KC_SLSH,        KC_RSFT,
                                         ALT_T(KC_ESC),  KC_RALT,        KC_LGUI,        TG(MOS),        MO(SYM),
         // right thumb
         KC_NO,          KC_NO,
         KC_NO,          KC_ENTER,       KC_SPC,
-        KC_RCTL,        KC_ENTER,       KC_SPC
+        CTL_T(KC_BSPC), KC_ENTER,       KC_SPC
     ),
 /* Keymap 1: Symbol Layer
  *
