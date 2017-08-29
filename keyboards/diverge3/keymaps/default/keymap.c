e29e155e95d6fe991a79056a28c06849d324c32d
Rainer Clasen <bj@zuto.de>
2017-08-29 17:39:16 +0200

    fix diverg keymap

--- keyboards/diverge3/keymaps/default/keymap.c
+++ keyboards/diverge3/keymaps/default/keymap.c
@@ -37,19 +37,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BASE] = KEYMAP( \
         // left hand
         KC_GRV,         KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           KC_DEL,
-        KC_TAB,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           KC_NO,
-        KC_NO,          KC_A,           KC_S,           KC_D,           KC_F,           KC_G,           KC_NO,
+        KC_TAB,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           KC_F1,
+        KC_F2,          KC_A,           KC_S,           KC_D,           KC_F,           KC_G,           KC_F3,
         KC_LSFT,        KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,           LSFT(KC_INS),
-        KC_F13,         KC_NO,          KC_LGUI,        KC_LALT,        KC_LCTL,
-                                                                        KC_SPC,         KC_NO,          KC_ESC,
+        KC_F4,          KC_F5,          KC_LGUI,        KC_LALT,        KC_LCTL,        KC_SPC,         KC_F6,          KC_ESC,
 
         // right hand
-        KC_PSCR,        KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_MINS,
-        KC_NO,          KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_NO,
-        KC_NO,          KC_H,           KC_J,           KC_K,           KC_L,           KC_NO,          KC_NO,
-        KC_F13,         KC_N,           KC_M,           KC_COMM,        KC_DOT,         KC_NO,          KC_LSFT,
-                                        KC_LSFT,        KC_LALT,        KC_LGUI,        KC_RALT,        KC_NO,
-        KC_TAB,         KC_BSPC,        KC_ENTER
+                        KC_PSCR,        KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_MINS,
+                        KC_F7,          KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_F8,
+                        KC_F9,          KC_H,           KC_J,           KC_K,           KC_L,           KC_F10,         KC_F11,
+                        KC_F12,         KC_N,           KC_M,           KC_COMM,        KC_DOT,         KC_F13,         KC_LSFT,
+        KC_TAB,         KC_BSPC,        KC_ENTER,       KC_LSFT,        KC_LALT,        KC_LGUI,        KC_RALT,        KC_F14
  ),
 
 };
