95c3450cf1942f5ce9ce93bb543de26c123d708b
Rainer Clasen <bj@zuto.de>
2018-06-13 20:35:20 +0200

    white space cleanup/formatting

--- keyboards/myatreus/keymaps/default/keymap.c
+++ keyboards/myatreus/keymaps/default/keymap.c
@@ -32,16 +32,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [BASE] = KEYMAP( \
         // left hand
-        KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,
-        KC_A,           KC_S,           KC_D,           KC_F,           KC_G,
-        KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,
-        KC_TAB,         XL_MOS,         XM_LALT,        XM_LCTL,        XM_LSFT,        XL_NAV,
+        KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,
+        KC_A,    KC_S,    KC_D,    KC_F,    KC_G,
+        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,
+        KC_TAB,  XL_MOS,  XM_LALT, XM_LCTL, XM_LSFT, XL_NAV,
 
         // right hand
-                        KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,
-                        KC_H,           KC_J,           KC_K,           KC_L,           KC_COMP,
-                        KC_N,           KC_M,           KC_COMM,        KC_DOT,         KC_ENTER,
-        KC_BSPC,        KC_SPC,         XL_SYM,         XL_MOS,         XM_RCTL,        XM_LGUI
+                 KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
+                 KC_H,    KC_J,    KC_K,    KC_L,    KC_COMP,
+                 KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_ENTER,
+        KC_BSPC, KC_SPC,  XL_SYM,  XL_MOS,  XM_RCTL, XM_LGUI
  ),
 /* Keymap 1: Symbol Layer
  *
--- keyboards/mydiverge/keymaps/default/keymap.c
+++ keyboards/mydiverge/keymaps/default/keymap.c
@@ -34,18 +34,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [BASE] = KEYMAP( \
         // left hand
-        KC_GRV,         KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           XXXXXXX,
-        XXXXXXX,        KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           XM_GHK,
-        KC_TAB,         KC_A,           KC_S,           KC_D,           KC_F,           KC_G,           LSFT(KC_INS),
-        KC_TAB,         KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,           KC_APP,
-        XM_LSFT,        KC_TAB,         XL_MOS,         XM_LALT,        XM_LCTL,        XM_LSFT,        XL_NAV,         KC_ESC,
+        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    XXXXXXX,
+        XXXXXXX, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    XM_GHK,
+        KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    LSFT(KC_INS),
+        KC_TAB,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_APP,
+        XM_LSFT, KC_TAB,  XL_MOS,  XM_LALT, XM_LCTL, XM_LSFT, XL_NAV,  KC_ESC,
 
         // right hand
-                        XXXXXXX,        KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_VOLU,
-                        KC_PSCR,        KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_VOLD,
-                        KC_COMP,        KC_H,           KC_J,           KC_K,           KC_L,           KC_COMP,        XXXXXXX,
-                        KC_BASE,        KC_N,           KC_M,           KC_COMM,        KC_DOT,         KC_ENTER,       XXXXXXX,
-        KC_RALT,        KC_BSPC,        KC_SPC,         XL_SYM,         XL_MOS,         XM_RCTL,        XM_LGUI,        KC_RSFT
+                 XXXXXXX, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_VOLU,
+                 KC_PSCR, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_VOLD,
+                 KC_COMP, KC_H,    KC_J,    KC_K,    KC_L,    KC_COMP, XXXXXXX,
+                 KC_BASE, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_ENTER,XXXXXXX,
+        KC_RALT, KC_BSPC, KC_SPC,  XL_SYM,  XL_MOS,  XM_RCTL, XM_LGUI, KC_RSFT
  ),
 /* Keymap 1: Symbol Layer
  *
