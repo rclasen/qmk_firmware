d11b86491261d4afc01e130c6e3f6231e41d4c5f
Rainer Clasen <bj@zuto.de>
2020-04-14 18:38:38 +0200

    ergodash: iris compatible thumb cluster

--- keyboards/ergodash/rev1/keymaps/rclasen/keymap.c
+++ keyboards/ergodash/rev1/keymaps/rclasen/keymap.c
@@ -34,7 +34,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * | lsft |   z  |   x  |   c  |   v  |   b  |
  * |------+------+------+------+------+------+-------------.
  * |      | tab  |      |      |      | fMOS | lsft | fNAV |
- * '---------------------------'      '--------------------'
+ * '--------------------'      '---------------------------'
 
  *        .------------------------------------------------.
  *        |      |   6  |   7  |   8  |   9  |   0  |      |
@@ -46,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *               |   n  |   m  |   ,  |   ,  | enter|      |
  * .-------------+------+------+------+------+------+------|
  * | BSPC |SPACE | fSYM |      |      | rctl | lgui | rsft |
- * '--------------------'      '---------------------------'
+ * '---------------------------'      '--------------------'
  */
 
 [BASE] = KEYMAP_HAND( \
@@ -55,14 +55,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         XM_GHK,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    XM_GHK,
         KC_TAB,  TX_A,    TX_S,    TX_D,    TX_F,    KC_G,    S_INS,
         XM_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,
-        XXXXXXX, KC_TAB,  XXXXXXX, XXXXXXX,          XL_MOS,  XM_LSFT, XL_NAV,
+        XXXXXXX, KC_TAB,  XXXXXXX,          XL_MOS,  XM_LSFT, XL_NAV,  XXXXXXX,
 
         // right hand
                  XXXXXXX, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    XXXXXXX,
                  KC_PSCR, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_PSCR,
                  MC_BASE, KC_H,    TX_J,    TX_K,    TX_L,    TX_COMP, XXXXXXX,
                           KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_ENTER,XXXXXXX,
-        KC_BSPC, KC_SPC,  XL_SYM,           XM_RCTL, XM_LGUI, XM_RSFT, XXXXXXX
+        XXXXXXX, KC_BSPC, KC_SPC,  XL_SYM,           XM_RCTL, XM_LGUI, XM_RSFT
  ),
 
 /* keymap: Symbol Layer
@@ -77,7 +77,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      |  #   |  $   |  |   |  ~   |   `  |
  * |------+------+------+------+------+------+-------------.
  * |      |      |      |      |      |      |      |      |
- * '---------------------------'      '--------------------'
+ * '--------------------'      '---------------------------'
 
  *        ,------------------------------------------------.
  *        |      |      |      |      |      |      |      |
@@ -89,7 +89,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *               |  +   |  %   |  "   |  '   |  ;   |      |
  * .-------------+------+------+------+------+------+------|
  * |      |      |      |      |      |      |      |      |
- * '--------------------'      '---------------------------'
+ * '---------------------------'      '--------------------'
  */
 
 [SYM] = KEYMAP_HAND(
@@ -98,14 +98,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        _______, KC_AT,   KC_UNDS, KC_LBRC, KC_RBRC, KC_CIRC, _______,
        _______, KC_BSLS, KC_SLSH, KC_LCBR, KC_RCBR, KC_ASTR, _______,
        _______, KC_HASH, KC_DLR,  KC_PIPE, KC_TILD, KC_GRV,
-       _______, _______, _______, _______,          _______, _______, _______,
+       _______, _______, _______,          _______, _______, _______, _______,
 
        // right hand
                 _______, _______, _______, _______, _______, _______, _______,
                 _______, KC_EXLM, KC_LABK, KC_RABK, KC_EQL,  KC_AMPR, _______,
                 _______, KC_QUES, KC_LPRN, KC_RPRN, KC_MINS, KC_COLN, _______,
                          KC_PLUS, KC_PERC, KC_DQT,  KC_QUOT, KC_SCLN, _______,
-       _______, _______, _______,          _______, _______, _______, _______
+       _______, _______, _______, _______,          _______, _______, _______
 ),
 
 /* keymap: navigation and keypad
@@ -120,7 +120,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      | Esc  | sIns | Ins  | Enter| space|
  * |------+------+------+------+------+------+-------------.
  * |      | GHK  |      |      |      |      |      |      |
- * '---------------------------'      '--------------------'
+ * '--------------------'      '---------------------------'
 
  *        .------------------------------------------------.
  *        |      | Tab  |NumLck|  /   |  *   |      |      |
@@ -131,8 +131,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *        '------+------+------+------+------+------+------|
  *               |  :   |  1   |  2   |  3   | Enter|      |
  * .-------------+------+------+------+------+------+------|
- * |      |      |      |      |  0   |      |      |      |
- * '--------------------'      '---------------------------'
+ * |      |      |      |  0   |      |      |      |      |
+ * '---------------------------'      '--------------------'
  */
 
 [NAV] = KEYMAP_HAND(
@@ -140,14 +140,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        _______, KC_PGUP, KC_BSPC, KC_UP,   KC_DEL,  KC_PGDN, _______,
        _______, KC_HOME, KC_LEFT, KC_DOWN, KC_RIGHT,KC_END,  _______,
        _______, KC_ESC,  S_INS,   KC_INS,  KC_ENTER,KC_SPACE,
-       _______, XM_GHK,  _______, _______,          _______, _______, _______,
+       _______, XM_GHK,  _______,          _______, _______, _______, _______,
 
        // right hand
                 _______, KC_TAB,  KC_NLCK, KC_SLSH, KC_ASTR, _______, _______,
                 _______, MC_MINS, MC_7,    MC_8,    MC_9,    MC_PLUS, _______,
                 _______, MC_DOT,  TX_4,    TX_5,    TX_6,    TX_COMM, _______,
                          MC_COLN, MC_1,    MC_2,    MC_3,    KC_ENTER,_______,
-       _______, _______, _______,          KC_0,    _______, _______, _______
+       _______, _______, _______, KC_0,             _______, _______, _______
 ),
 
 /* keymap: Media and mouse keys
@@ -162,7 +162,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      |  F11 |  F12 | MENU | print| CAPS |
  * |------+------+------+------+------+------+-------------.
  * |      |      |      |      |      |      |      |      |
- * '---------------------------'      '--------------------'
+ * '--------------------'      '---------------------------'
 
  *        .------------------------------------------------.
  *        |      |      |      |      |      |      |      |
@@ -174,7 +174,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *               | mprev| mstop| mplay| mnext| Rclk |      |
  * .-------------+------+------+------+------+------+------|
  * |      |      |      |      |      | VER  | RESET|      |
- * '--------------------'      '---------------------------'
+ * '---------------------------'      '--------------------'
  */
 
 [MOS] = KEYMAP_HAND(
@@ -182,14 +182,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_RST,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_RST,
        MC_VER,  TX_F6,   TX_F7,   TX_F8,   TX_F9,   KC_F10,  MC_VER,
        _______, KC_F11,  KC_F12,  KC_APP,  KC_PSCR, KC_CAPS,
-       _______, _______, _______, _______,          _______, _______, _______,
+       _______, _______, _______,          _______, _______, _______, _______,
 
        // right hand
                 _______, _______, _______, _______, _______, _______, _______,
                 KC_RST,  KC_NLCK, KC_VOLD, KC_VOLU, KC_MUTE, KC_BTN3, KC_RST,
                 MC_VER,  KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_BTN1, MC_VER,
                          KC_MPRV, KC_MSTP, KC_MPLY, KC_MNXT, KC_BTN2, _______,
-       _______, _______, _______,          _______, MC_VER,  KC_RST,  _______
+       _______, _______, _______, _______,          MC_VER,  KC_RST,  _______
 ),
 
 };
