3ac3fa2186d2de76221db04a5d814f0aded3bfb1
Rainer Clasen <bj@zuto.de>
2023-04-01 20:08:48 +0200

    adjusted for DFU loader on tmk mini / elite c

--- mykeymap.mk
+++ mykeymap.mk
@@ -12,63 +12,22 @@ NAMES	?= default
 build:
 	make -C $(TOP) $(BOARD):$(MAP)
 
-pending	:= $(patsubst %,.flash-%,$(NAMES))
-pending: $(pending)
-$(pending): .flash-%: rules.mk config.h keymap.c
-	@echo "************ $(BOARD): $@"
+flash: $(FLASH)
 
-flash	:= $(patsubst %,flash-%,$(NAMES))
-$(FLASH) flash: $(flash)
-$(flash): flash-%: build
-	@echo "************ flashing $(BOARD): $@"
-	make -C $(TOP) $(BOARD):$(MAP):$(FLASH)
-	date > .$@
-
-eeleft	:= $(patsubst %,eeleft-%,$(filter %left,$(NAMES)))
+eeleft	:= $(patsubst %,$(FLASH)-split-%,$(filter %left,$(NAMES)))
 eeleft: $(eeleft)
-eeright	:= $(patsubst %,eeright-%,$(filter %right,$(NAMES)))
+eeright	:= $(patsubst %,$(FLASH)-split-%,$(filter %right,$(NAMES)))
 eeright: $(eeright)
 eehand: eeleft eeright
 
-splitdir	:= $(TOP)/quantum/split_common
+all	:= $(FLASH) $(eeleft) $(eeright)
+$(all):
+	@echo "************ flashing $(BOARD):$(MAP):$@"
+	make -C $(TOP) $(BOARD):$(MAP):$@
+	date > .$@
 
-$(eeleft): eeleft-%:
-	@echo "************ eehand left $(BOARD): $@"
-	@printf "Detecting USB port, reset your controller now."; \
-		ls /dev/tty* > /tmp/1; \
-		while [ -z $$USB ]; do \
-			sleep 0.5; \
-			printf "."; \
-			ls /dev/tty* > /tmp/2; \
-			USB=`comm -13 /tmp/1 /tmp/2 | grep -o '/dev/tty.*'`; \
-			mv /tmp/2 /tmp/1; \
-		done; \
-		echo ""; \
-		echo "Detected controller on USB port at $$USB"; \
-		if grep -q -s 'MINGW\|MSYS' /proc/version; then \
-			USB=`echo "$$USB" | perl -pne 's/\/dev\/ttyS(\d+)/COM.($$1+1)/e'`; \
-			echo "Remapped MSYS2 USB port to $$USB"; \
-		fi; \
-		sleep 1; \
-		avrdude -p atmega32u4 -P $$USB -c avr109 -U eeprom:w:$(splitdir)/eeprom-lefthand.eep
-
-$(eeright): eeright-%:
-	@echo "************ eehand right $(BOARD): $@"
-	@printf "Detecting USB port, reset your controller now."; \
-		ls /dev/tty* > /tmp/1; \
-		while [ -z $$USB ]; do \
-			sleep 0.5; \
-			printf "."; \
-			ls /dev/tty* > /tmp/2; \
-			USB=`comm -13 /tmp/1 /tmp/2 | grep -o '/dev/tty.*'`; \
-			mv /tmp/2 /tmp/1; \
-		done; \
-		echo ""; \
-		echo "Detected controller on USB port at $$USB"; \
-		if grep -q -s 'MINGW\|MSYS' /proc/version; then \
-			USB=`echo "$$USB" | perl -pne 's/\/dev\/ttyS(\d+)/COM.($$1+1)/e'`; \
-			echo "Remapped MSYS2 USB port to $$USB"; \
-		fi; \
-		sleep 1; \
-		avrdude -p atmega32u4 -P $$USB -c avr109 -U eeprom:w:$(splitdir)/eeprom-righthand.eep
+stamps := $(patsubst %,.%,$(all))
+pending: $(stamps)
+$(stamps): .%: rules.mk config.h keymap.c
+	@echo "************ $(BOARD): $@"
 
